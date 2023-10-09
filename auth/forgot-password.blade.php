b7c487d946cced88871d62d107797476ed97e006
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-09 14:44:48 +0300

    Update forgot-password.blade.php

--- auth/forgot-password.blade.php
+++ auth/forgot-password.blade.php
@@ -16,8 +16,7 @@
                         <label for="email"
                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{!! __('auth.email') !!}</label>
                         <input type="email" name="email" id="email"
-                               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                               placeholder="{!! __('auth.enter_email') !!}" required="">
+                               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="{!! __('auth.enter_email') !!}" required="">
                     </div>
 
                     <button type="submit"
