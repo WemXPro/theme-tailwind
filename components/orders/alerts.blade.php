5e2e73af1e3922ddfffe5f617518e1180fe73699
Mubeen142 <mubeenullah323@gmail.com>
2023-10-31 16:26:37 +0200

    add error log

--- components/orders/alerts.blade.php
+++ components/orders/alerts.blade.php
@@ -58,3 +58,25 @@
         </div>
     </div>
 @endif
+
+@if(ErrorLog::where('order_id', $order->id)->where('severity','!=','RESOLVED')->count() !== 0)
+    <div id="alert-additional-content-2"
+         class="p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+         role="alert">
+        <div class="flex items-center">
+            <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                      clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">{!! __('client.important') !!}</span>
+            <h3 class="text-lg font-medium">{!! __('client.ptero_alerts_failed_server') !!}</h3>
+        </div>
+        <div class="mt-2 mb-4 text-sm">
+            {!! __('client.ptero_alerts_failed_server_desc') !!}
+        </div>
+        <div class="flex">
+        </div>
+    </div>
+@endif
