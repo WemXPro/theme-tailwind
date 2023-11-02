6202a2975ab1f8480a07aa6c7df816cc4d837e6a
Mubeen142 <mubeenullah323@gmail.com>
2023-11-02 21:29:44 +0200

    Update wrapper.blade.php

--- layouts/wrapper.blade.php
+++ layouts/wrapper.blade.php
@@ -5,8 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="WemX Billing System">
-    <meta name="keywords" content="WemX Panel, Billing Panel,  @yield('keywords')">
+    <meta name="theme-color" content="#0045ff">
+    <meta property="og:title" content="@settings('app_name', 'WemX')">
+    <meta property="og:description" content="Manage your orders with an easy-to-use Dashboard">
+    <meta property="og:image" content="@settings('logo', 'https://imgur.com/oJDxg2r.png')">
+    <meta name="keywords" content="wemx, WemX, WemX Pro, WHMCS, Billing,  @yield('keywords')">
     <meta name="author" content="WemX">
     <title>@yield('title') - @settings('app_name', 'WemX')</title>
     <link rel="icon" href="@settings('favicon', 'https://imgur.com/oJDxg2r.png')">
