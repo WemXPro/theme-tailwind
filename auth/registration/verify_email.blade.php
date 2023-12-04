7a502ab87b6b8c27b40259a9ca9345fe698b5905
Nabeel Mehdi Imrani <nabeelemraane09@gmail.com>
2023-12-04 19:01:50 +0500

    update

--- auth/registration/verify_email.blade.php
+++ auth/registration/verify_email.blade.php
@@ -113,16 +113,62 @@
                 </button>
             </div>
         </form>
-        <div class="mt-4">
-            <a href="{{ route('verification.change-email.form') }}"
-                class="text-primary-600 dark:text-primary-500 hover:underline">
-                <button type="submit"
-                    class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4
-                    focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5
-                    py-2.5 sm:py-3.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    {!! __('Change Email') !!}
-                </button>
-            </a>
+
+
+
+        <!-- Modal toggle -->
+        <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
+            class="mt-2 text-primary-600 dark:text-primary-500 font-bold">
+            {!! __('Change Email') !!}
+        </button>
+        <!-- Main modal -->
+        <div id="authentication-modal" tabindex="-1" aria-hidden="true"
+            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-md max-h-full">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <!-- Modal header -->
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                            {!! __('Change Email') !!}
+                        </h3>
+                        <button type="button"
+                            class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            data-modal-hide="authentication-modal">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <!-- Modal body -->
+                    <div class="p-4 md:p-5">
+                        <form class="space-y-4" action="{{ route('verification.change-email') }}" method="POST">
+                            @csrf
+                            <div>
+                                <label for="new_email"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Email</label>
+                                <input type="email" name="new_email" id="new_email"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                    placeholder="new-email@example.com" required>
+                                @error('new_email')
+                                    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                                @enderror
+                            </div>
+
+
+
+                            <button type="submit"
+                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Change Email
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
