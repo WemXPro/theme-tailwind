e73a957a97e3052c8599414e45b054c889b669a0
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-01-03 01:14:57 +0200

    associative array options

--- store/packages/view.blade.php
+++ store/packages/view.blade.php
@@ -140,18 +140,14 @@
                                 {!! $field['name'] !!} {!!  isset($field['required']) && $field['required'] ? '<span class="text-red-500">*</span>' : '' !!}
                             </label>
                             @if($field['type'] == 'select')
-                                <select
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    tabindex="-1" aria-hidden="true"
-                                    name="{{ $field['key'] }}"
-                                    id="{{ $field['key'] }}"
+                                <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    tabindex="-1" aria-hidden="true" name="{{ $field['key'] }}" id="{{ $field['key'] }}"
                                     @if(isset($field['disabled']) AND $field['disabled']) disabled @endif
-                                    @if(isset($field['multiple']) AND $field['multiple']) multiple @endif
-                                >
+                                    @if(isset($field['multiple']) AND $field['multiple']) multiple @endif >
                                     @foreach($field['options'] ?? [] as $key => $option)
                                         <option value="{{ $key }}"
-                                                @if(in_array($key, (array) $package->data(Str::remove("[]", $field['key']), $field['default_value'] ?? ''))) selected @endif
-                                        >{{ $option }}</option>
+                                                @if(in_array($key, (array) $package->data(Str::remove("[]", $field['key']), $field['default_value'] ?? ''))) selected @endif >{{ $option }}
+                                        </option>
                                     @endforeach
                                 </select>
                             @elseif($field['type'] == 'bool')
@@ -160,22 +156,18 @@
                                         <input type="hidden" name="{{ $field['key'] }}" value="0">
                                     @endif
                                     <input type="checkbox" name="{{ $field['key'] }}" value="{{$field['default_value'] ?? '0'}}" class="sr-only peer"
-                                           @if($package->data($field['key'], $field['default_value'] ?? '0')) checked @endif
+                                           @if($package->data($field['key'], $field['default_value'] ?? '0')) checked
+                                           @endif
                                            @if(isset($field['disabled']) AND $field['disabled']) disabled @endif>
-                                    <div
-                                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                    <span
-                                        class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{!! $field['name'] !!}</span>
+                                    <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{!! $field['name'] !!}</span>
                                 </label>
                             @else
-                                <input
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    type="{{ $field['type'] }}"
-                                    name="{{ $field['key'] }}"
-                                    id="{{ $field['key'] }}"
+                                <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    type="{{ $field['type'] }}" name="{{ $field['key'] }}" id="{{ $field['key'] }}"
                                     @isset($field['min']) min="{{$field['min']}}" @endisset
                                     @isset($field['max']) max="{{$field['max']}}" @endisset
-                                    value="{{ $package->data($field['key'], $field['default_value'] ?? '') }}"
+                                    value="{{getValueByKey($field['key'], $package->data, $field['default_value'] ?? '')}}"
                                     placeholder="@isset($field['placeholder']){{$field['placeholder']}} @else{{ $field['name'] }} @endisset"
                                     @if(in_array('required', $field['rules'])) required="" @endif
                                     @if(isset($field['disabled']) AND $field['disabled']) disabled @endif>
@@ -189,7 +181,7 @@
             </div>
         </div>
     @else
-            @includeIf(Theme::serviceView($package->service, 'props.checkout-options'))
+        @includeIf(Theme::serviceView($package->service, 'props.checkout-options'))
     @endif
 
     <div class="relative mt-8 p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
