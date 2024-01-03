48058ea36e4cdd606e18e4fa33a7416fdc7037ae
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-01-03 03:00:11 +0200

    Update view.blade.php

--- store/packages/view.blade.php
+++ store/packages/view.blade.php
@@ -146,7 +146,7 @@
                                     @if(isset($field['multiple']) AND $field['multiple']) multiple @endif >
                                     @foreach($field['options'] ?? [] as $key => $option)
                                         <option value="{{ $key }}"
-                                                @if(in_array($key, (array) $package->data(Str::remove("[]", $field['key']), $field['default_value'] ?? ''))) selected @endif >{{ $option }}
+                                                @if(in_array($key, (array) getValueByKey($field['key'], $package->data, $field['default_value'] ?? ''))) selected @endif >{{ $option }}
                                         </option>
                                     @endforeach
                                 </select>
@@ -156,7 +156,7 @@
                                         <input type="hidden" name="{{ $field['key'] }}" value="0">
                                     @endif
                                     <input type="checkbox" name="{{ $field['key'] }}" value="{{$field['default_value'] ?? '0'}}" class="sr-only peer"
-                                           @if($package->data($field['key'], $field['default_value'] ?? '0')) checked
+                                           @if(getValueByKey($field['key'], $package->data, $field['default_value'] ?? '0')) checked
                                            @endif
                                            @if(isset($field['disabled']) AND $field['disabled']) disabled @endif>
                                     <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
