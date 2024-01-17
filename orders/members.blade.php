0b413b3031e23a0997304d98536082025b3e5bbb
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-01-17 23:40:40 +0200

    fix

--- orders/members.blade.php
+++ orders/members.blade.php
@@ -193,7 +193,7 @@
             @foreach($order->getService()->permissions()->all() as $key => $permission)
             <div class="flex mb-3">
                 <div class="flex items-center h-5">
-                    <input id="permission-{{$key}}" name="permissions[{{$key}}]" aria-describedby="permission-{{$key}}-text" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <input id="permission-{{$key}}" name="permissions[{{$key}}]" aria-describedby="permission-{{$key}}-text" type="checkbox" value="@if(isset($permission['contains']) and $permission['contains']) contains @endif" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                 </div>
                 <div class="ml-2 text-sm">
                     <label for="permission-{{$key}}" class="font-medium text-gray-900 dark:text-gray-300">{{ str_replace("_", " ", $key) }}</label>
