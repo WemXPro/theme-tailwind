d8af045c3829a994351e220b88ad902471cb29ca
Mubeen142 <mubeenullah323@gmail.com>
2024-01-22 21:59:53 +0200

    Update view.blade.php

--- store/packages/view.blade.php
+++ store/packages/view.blade.php
@@ -113,12 +113,71 @@
                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                        placeholder="i.e example.com">
                 <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                    {!! __('client.enter_domain_helper') !!}/p>
+                    {!! __('client.enter_domain_helper') !!}
+                </p>
             </div>
         </div>
     @endif
 
+    @if($package->configOptions->count() > 0 )
+    <div class="relative mt-8 p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 mb-6">
+        <div class="custom-note">
+            <div class="flex justify-between mb-3 rounded-t sm:mb-3">
+                <div class="text-lg text-gray-900 md:text-xl dark:text-white">
+                    <h3 class="font-semibold">Configurable Options</h3>
+                    <p class="text-sm text-gray-500 dark:text-gray-400">
+                        Configurable options allow you to customize your product.
+                    </p>
+                </div>
+            </div>
+            {{-- load configurable options --}}
+            @foreach($package->configOptions as $option)
+                @if($option->type == 'select') 
+                    <div class="mb-4">
+                        <label for="{{ $option->key }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $option->data['label'] ?? 'Undefined' }}</label>
+                        <select id="{{ $option->key }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                          @foreach($option->data['options'] ?? [] as $optionData)
+                            <option value="{{ $optionData['value'] }}">{{ $optionData['name'] }} ({{ price(20) }} @ <span id="">monthly</span>)</option>
+                          @endforeach
+                        </select>
+                        <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">
+                            {{ $option->data['description'] ?? 'Undefined' }}
+                        </p>
+                    </div>
+                @elseif($option->type == 'number')
+                <div class="mb-4">
+                    <label for="quantity-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $option->data['label'] ?? 'Undefined' }}</label>
+                    <div class="relative flex items-center max-w-[8rem]">
+                        <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                            <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16"/>
+                            </svg>
+                        </button>
+                        <input type="text" id="quantity-input" min="{{ $option->data['min'] ?? '0' }}" min="{{ $option->data['max'] ?? '0' }}" value="{{ $option->data['default_value'] ?? '0' }}" class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required>
+                        <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                            <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">{{ $option->data['label'] ?? 'description' }}</p>
+                </div>
+                @elseif($option->type == 'range')
 
+                <div class="relative mb-6">
+                    <label for="quantity-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $option->data['label'] ?? 'Undefined' }}</label>
+                    <input id="labels-range-input" type="range" value="{{ $option->data['default_value'] ?? 0 }}" min="{{ $option->data['min'] ?? 0 }}" max="{{ $option->data['max'] ?? 10 }}" step="{{ $option->data['step'] ?? 1 }}" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                    <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">Min ({{ $option->data['min'] ?? 0 }})</span>
+                    {{-- <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$500</span>
+                    <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">$1000</span> --}}
+                    <span class="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">Max ({{ $option->data['max'] ?? 0 }})</span>
+                </div>
+                
+                @endif
+            @endforeach
+        </div>
+    </div>
+    @endif
 
     @if($package->service()->hasCheckoutConfig($package))
         <div class="relative mt-8 p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 mb-6">
