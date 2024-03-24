4372ba7b8241f442d2de9ae496ae05292c7d71e3
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-03-24 18:07:21 +0200

    Update index.blade.php

--- dashboard/index.blade.php
+++ dashboard/index.blade.php
@@ -104,8 +104,7 @@
                                                            class="text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 h-4 w-4 rounded border-gray-300 bg-gray-100 focus:ring-2 dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700">
                                                     <label for="{{ $status }}"
                                                            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">{{ __('client.' . $status) }}
-                                                        ({{ auth()->user()->orders()->where('status', $status)->count() }}
-                                                        )
+                                                        ({{ auth()->user()->orders()->where('status', $status)->count() }})
                                                     </label>
                                                 </li>
                                             @endforeach
