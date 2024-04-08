305b03d49c5d3b50dd6c9cf8fc9b9ea2d2604bc3
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2024-04-08 13:24:43 +0200

    Update address.blade.php

--- auth/registration/address.blade.php
+++ auth/registration/address.blade.php
@@ -40,7 +40,7 @@
                             <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
                         </svg>
                     </div>
-                    <input type="text" id="phone-input" name="phone_number" value="{{ auth()->user()->address->phone_number ?? '' }}" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-456-7890" required />
+                    <input type="text" id="phone-input" name="phone_number" value="{{ auth()->user()->address->phone_number ?? '' }}" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="123-456-7890" required />
                 </div>
             </div>
             @endif
@@ -56,7 +56,7 @@
                     {{ __('client.country') }}
                 </label>
                 <select id="countries" name="country" required
-                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500">
                     @foreach (config('utils.countries') as $key => $country)
                         <option value="{{ $key }}" @if(request()->header('cf-ipcountry') == $key) selected @endif>
                             {{ $country }}</option>
