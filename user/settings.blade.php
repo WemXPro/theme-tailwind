8c3f3f6845ca25456757582638bb2aa16f05c5ab
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2024-07-03 10:28:00 +0200

    Update settings.blade.php

--- user/settings.blade.php
+++ user/settings.blade.php
@@ -320,7 +320,7 @@
                             <select id="countries" name="country"
                                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500">
                                 @foreach (config('utils.countries') as $key => $country)
-                                    <option value="{{ $key }}" @if (auth()->user()->address->country ?? '' == $key) selected @endif>
+                                    <option value="{{ $key }}" @if (auth()->user()->address->country == $key) selected @endif>
                                         {{ $country }}</option>
                                 @endforeach
                             </select>
