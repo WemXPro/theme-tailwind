34997799e7091f575121028e9d0a592ca944711b
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2024-10-13 01:30:13 +0200

    Update manage.blade.php

--- orders/manage.blade.php
+++ orders/manage.blade.php
@@ -12,18 +12,20 @@
     @endforeach
 
     @if($order->isRecurring() AND !$order->hasActiveSubscription())
-    <div class="mb-4 flex rounded-lg bg-gray-50 p-4 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        <label class="flex justify-between w-full items-center cursor-pointer" onclick="enableBalanceRenew()">
-            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('client.auto_renew_with_balance') }}</span>
+        @if($order->package->settings('allow_auto_balance_renewal', true))
+        <div class="mb-4 flex rounded-lg bg-gray-50 p-4 dark:bg-gray-800" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <label class="flex justify-between w-full items-center cursor-pointer" onclick="enableBalanceRenew()">
+                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('client.auto_renew_with_balance') }}</span>
 
-            <div>
-                <input type="checkbox" value="" class="sr-only peer" @if($order->auto_balance_renew) checked @endif/>
-                <div
-                    class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600"
-                ></div>
-            </div>
-        </label>
-    </div>
+                <div>
+                    <input type="checkbox" value="" class="sr-only peer" @if($order->auto_balance_renew) checked @endif/>
+                    <div
+                        class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600"
+                    ></div>
+                </div>
+            </label>
+        </div>
+        @endif
     @endif
     
     <div id="service">
