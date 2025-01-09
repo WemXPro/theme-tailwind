57324b0c4d49ca928c0e8f6ef7b274ae51c41e31
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2025-01-09 12:00:29 +0200

    Update page.blade.php

--- page.blade.php
+++ page.blade.php
@@ -7,14 +7,18 @@
 @endsection
 
 @section('container')
-    <main class="pb-16 pt-8 lg:pb-24 lg:pt-16">
-        <article class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-2xl">
-            <header class="not-format mb-4 lg:mb-6">
-                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:mb-6 lg:text-4xl">
-                    {{ $page->title }}
-                </h1>
-            </header>
-            {!! $page->content !!}
-        </article>
-    </main>
+    @if($page->basic_page)
+        {!! $page->content !!}
+    @else
+        <main class="pb-16 pt-8 lg:pb-24 lg:pt-16">
+            <article class="format format-sm sm:format-base lg:format-lg format-blue dark:format-invert mx-auto w-full max-w-2xl">
+                <header class="not-format mb-4 lg:mb-6">
+                    <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 dark:text-white lg:mb-6 lg:text-4xl">
+                        {{ $page->title }}
+                    </h1>
+                </header>
+                {!! $page->content !!}
+            </article>
+        </main>
+    @endif
 @endsection
